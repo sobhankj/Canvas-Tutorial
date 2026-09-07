4ce8e071a30a10da846094003e567bf96290d349
Sobhan Kooshki <sobhankj.tehran@gmail.com>
2026-09-07 16:52:04 +0330

    create human with fillRect

---
+++ Session2 setup/script.js
@@ -0,0 +1,67 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'hsl(37, 89.30%, 89.00%)';
+ctx.fillRect(550, 50 , 290 , 290);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(590 , 80 , 70 , 70);
+
+ctx.fillRect(725, 80 , 70 , 70);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(615 , 100 , 20 , 30);
+
+ctx.fillRect(750 , 100 , 20 , 30);
+
+ctx.fillStyle = 'rgb(81, 48, 22)';
+ctx.fillRect(663 , 160 , 60 , 90);
+
+ctx.fillStyle = 'rgb(123, 0, 0)';
+ctx.fillRect(600 , 270 , 200 , 35);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(550, 50 , 290 , 20);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(605, 293 , 15 , 12);
+ctx.fillRect(630, 293 , 15 , 12);
+ctx.fillRect(680, 293 , 15 , 12);
+ctx.fillRect(705, 293 , 15 , 12);
+ctx.fillRect(730, 293 , 15 , 12);
+ctx.fillRect(755, 293 , 15 , 12);
+ctx.fillRect(780, 293 , 15 , 12);
+
+ctx.fillRect(605, 270 , 15 , 12);
+ctx.fillRect(630, 270 , 15 , 12);
+ctx.fillRect(655, 270 , 15 , 12);
+ctx.fillRect(680, 270 , 15 , 12);
+ctx.fillRect(705, 270 , 15 , 12);
+ctx.fillRect(755, 270 , 15 , 12);
+ctx.fillRect(780, 270 , 15 , 12);
+
+ctx.fillStyle = 'rgb(0, 6, 62)';
+ctx.fillRect(420, 340 , 560 , 300);
+
+ctx.fillStyle = 'rgb(152, 33, 0)';
+ctx.fillRect(420, 340 , 50 , 300);
+ctx.fillRect(930, 340 , 50 , 300);
+
+ctx.fillStyle = 'rgb(8, 94, 0)';
+ctx.fillRect(690, 350 , 20 , 20);
+ctx.fillRect(690, 400 , 20 , 20);
+ctx.fillRect(690, 450 , 20 , 20);
+ctx.fillRect(690, 500 , 20 , 20);
+ctx.fillRect(690, 550 , 20 , 20);
+ctx.fillRect(690, 600 , 20 , 20);
+
+ctx.fillStyle = 'hsl(37, 89.30%, 89.00%)';
+ctx.fillRect(420, 610 , 50 , 60);
+ctx.fillRect(930, 610 , 50 , 60);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(520, 640 , 350 , 180);
+
+ctx.fillStyle = 'rgb(190, 92, 0)';
+ctx.fillRect(460, 820 , 180 , 60);
+ctx.fillRect(730, 820 , 180 , 60);
